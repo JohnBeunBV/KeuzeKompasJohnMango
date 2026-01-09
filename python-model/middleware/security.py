07b5097ff7a29cf56aa3aec926f9165061234991
S.k2004 <stormkroonen@hotmail.nl>
2026-01-09 15:50:37 +0100

    added postman test for the pyhton api and better security

--- python-model/middleware/security.py
+++ python-model/middleware/security.py
@@ -4,10 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def verify_api_key(x_api_key: str = Header(...)):
+def verify_api_key(x_api_key: str | None = Header(None)):
     expected = os.getenv("PYTHON_API_KEY")
-    if not expected or x_api_key != expected:
+
+    if not x_api_key:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or missing API key"
+            detail="Missing API key"
+        )
+
+    if not expected or x_api_key != expected:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid API key"
         )
